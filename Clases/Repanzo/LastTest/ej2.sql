9fe51600e8fab764217333b5685d6773ddf99b58
LautaroVelez <lautivelez28@gmail.com>
2023-11-27 21:38:50 -0300

    2 hecho broo

--- Clases/Repanzo/LastTest/ej2.sql
+++ Clases/Repanzo/LastTest/ej2.sql
@@ -1,11 +1,13 @@
+-- Active: 1687904736759@@127.0.0.1@3306@imdb
 DROP DATABASE IF EXISTS imdb;
      CREATE DATABASE imdb;
+USE imdb;
 
 CREATE TABLE IF NOT EXISTS film(
     film_id int PRIMARY KEY  AUTO_INCREMENT,
     title VARCHAR(50) NOT NULL,
     description VARCHAR(120),
-    release_year DATE,
+    release_year DATE
 );
 
 CREATE TABLE IF NOT EXISTS actor(
@@ -24,7 +26,7 @@ CREATE TABLE IF NOT EXISTS film_actor(
 ALTER TABLE film ADD last_update DATE AFTER release_year;
 ALTER TABLE actor ADD last_update DATE AFTER last_name;
 
-ALTER TABLE film_actor ADD FOREIGN KEY (film_id) REFERENCES film(filml_id);
+ALTER TABLE film_actor ADD FOREIGN KEY (film_id) REFERENCES film(film_id);
 ALTER TABLE film_actor ADD FOREIGN KEY (actor_id) REFERENCES actor(actor_id);
 
-INSERT INTO film
+INSERT INTO FILM (TITLE, DESCRIPTION, RELEASE_YEAR) VALUES('JACK SPARROW','ASDASDASDSADASDASDAS','1994');
