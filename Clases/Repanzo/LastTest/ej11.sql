cb35ab800027a2b3b6123cd2e2fa159152e39ac8
LautaroVelez <lautivelez28@gmail.com>
2023-12-19 21:08:15 -0300

    ej11 creado

---
+++ Clases/Repanzo/LastTest/ej11.sql
@@ -0,0 +1,19 @@
+--5--Encuentra todas las películas que están en el inventario pero nunca fueron alquiladas.
+--Muestra el título y el inventory_id.
+--Este ejercicio es complicado.
+--Sugerencia: utiliza subconsultas en el FROM y en el WHERE, o utiliza LEFT JOIN y pregunta si uno de los campos es nulo.
+
+--6--Genera un informe con:
+
+--Nombre del cliente (primero, último), ID de la tienda, título de la película,
+--cuándo se alquiló y se devolvió la película para cada uno de estos clientes
+--ordenado por store_id, apellido del cliente.
+
+--7Muestra las ventas por tienda (monto de películas alquiladas).
+
+--Muestra la ciudad, país, información del gerente y ventas totales (monto) de la tienda.
+--(Opcional) Utiliza CONCAT para mostrar la ciudad y el país, y el nombre y apellido del gerente.
+
+--8--¿Qué actor ha aparecido en la mayoría de las películas?
+
+--4--Encuentra todos los títulos de películas que no están en el inventario.
